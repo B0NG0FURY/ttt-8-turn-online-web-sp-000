b1357baf5a090d455939fadf9965d594aedb2b69
John Troutman <jtroutmanman@gmail.com>
2020-09-08 06:12:48 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -29,3 +29,13 @@ end
 def move(board, index, character = "X")
   board[index] = character
 end
+
+def turn
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move(board, index) == true
+    move(board, index)
+    display_board(board)
+  end
+end
